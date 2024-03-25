307a6c76adb84ce194d20b74c10ed8384a8dc7b6
CyberFruitSeeker <assortrock0@ASSORTROCK>
2024-03-25 18:47:03 +0900

    함수 포인터 코드 입력 실습 필요

---
+++ CPP_Practice_DirectX/001_Function/001_Function.cpp
@@ -0,0 +1,19 @@
+﻿// 001_Function.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+// Functional(함수 포인터)를 여기서 한번 실습해본다.
+
+#include <iostream>
+#include <functional>
+
+void TestFunction()
+{
+    std::cout << "함수 포인터 테스트 해봄~\n";
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
